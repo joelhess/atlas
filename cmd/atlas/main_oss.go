16602851f20e65356579d1c03165c6a99cf9c5bd
Ariel Mashraki <ariel@mashraki.co.il>
2025-12-04 15:41:28 +0200

    cmd/atlas: suggest standard build in case of errors

--- cmd/atlas/main_oss.go
+++ cmd/atlas/main_oss.go
@@ -8,6 +8,14 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+	"time"
+
+	"ariga.io/atlas/cmd/atlas/internal/cmdlog"
+	"ariga.io/atlas/cmd/atlas/internal/cmdstate"
 )
 
 func extendContext(ctx context.Context) (context.Context, error) {
@@ -20,5 +28,36 @@ func vercheckEndpoint(context.Context) string {
 
 // initialize is a no-op for the OSS version.
 func initialize(ctx context.Context) (context.Context, func(error)) {
-	return ctx, func(error) {}
+	return ctx, func(err error) {
+		if err == nil {
+			return
+		}
+		const errorsFileName = "community_error.json"
+		type prompt struct {
+			LastSuggested time.Time `json:"last_suggested"`
+		}
+		state := &cmdstate.File[prompt]{Name: errorsFileName}
+		prev, err := state.Read()
+		if err != nil || time.Since(prev.LastSuggested) < 24*time.Hour {
+			return
+		}
+		var (
+			b       strings.Builder
+			release = "curl -sSf https://atlasgo.sh | sh"
+		)
+		if runtime.GOOS == "windows" {
+			release = "https://release.ariga.io/atlas/atlas-windows-amd64-latest.exe"
+		}
+		fmt.Fprintf(&b, cmdlog.ColorCyan(fmt.Sprintf(`You're running the community build of Atlas, which may differ from the official version.
+If this error persists, try installing the official version as a troubleshooting step:
+
+  %s
+
+More installation options: https://atlasgo.io/docs#installation
+`, release)))
+		if err := cmdlog.WarnOnce(os.Stderr, b.String()); err == nil {
+			prev.LastSuggested = time.Now()
+			_ = state.Write(prev)
+		}
+	}
 }
